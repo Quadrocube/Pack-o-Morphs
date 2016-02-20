a20eea9b6df22a0eb8f3f5bd79df95b7ffe8e105
Kamil Talipov <kamiltalipov@gmail.com>
2016-02-20 16:23:55 +0300

    Refactor game.js

--- client/game.js
+++ client/game.js
@@ -1,104 +1,164 @@
-window.onload = function() {
-	
-	var game = new Phaser.Game("100%", "100%", Phaser.CANVAS, "", {preload: onPreload, create: onCreate});                
+window.onload = function() {	
+    var Game = new Phaser.Game("100%", "100%", Phaser.CANVAS, "", {preload: onPreload, create: onCreate});
 
-	var hexagonWidth = 35;
-	var hexagonHeight = 40;
-	var gridSizeX = 52;
-	var gridSizeY = 26;
-	var columns = [Math.ceil(gridSizeX/2),Math.floor(gridSizeX/2)];
-    var moveIndex;
-    var sectorWidth = hexagonWidth;
-    var sectorHeight = hexagonHeight/4*3;
-    var gradient = (hexagonHeight/4)/(hexagonWidth/2);
-    var marker;
-    var hexagonGroup;
+    function TGameWorld() {
+        var hexagonWidth = 35;
+        var hexagonHeight = 40;
+        var gridSizeX = 52;
+        var gridSizeY = 26;
+        var columns = [Math.ceil(gridSizeX / 2),Math.floor(gridSizeX / 2)];
+        var sectorWidth = hexagonWidth;
+        var sectorHeight = hexagonHeight / 4 * 3;
+        var gradient = (hexagonHeight / 4) / (hexagonWidth / 2);
     
-	function onPreload() {
-		game.load.image("hexagon", "arts/hexagon.png");
-		game.load.image("marker", "arts/marker.png");
-	}
-
-	function onCreate() {
-		hexagonGroup = game.add.group();
-		game.stage.backgroundColor = "#ffffff";
-	    for(var i = 0; i < gridSizeY/2; i ++){
-			for(var j = 0; j < gridSizeX; j ++){
-				if(gridSizeY%2==0 || i+1<gridSizeY/2 || j%2==0){
-					var hexagonX = hexagonWidth*j/2;
-					var hexagonY = hexagonHeight*i*1.5+(hexagonHeight/4*3)*(j%2);	
-					var hexagon = game.add.sprite(hexagonX,hexagonY,"hexagon");
+        this.GetHexagonWidth = function () {
+            return hexagonWidth;
+        };
+    
+        this.GetHexagonHeight = function () {
+            return hexagonHeight;
+        };
+    
+        this.GetGridSizeX = function () {
+            return gridSizeX;
+        };
+    
+        this.GetGridSizeY = function () {
+            return gridSizeY;
+        };
+        
+        this.GetColumns = function () {
+            return columns;
+        };
+        
+        this.GetSectorWidth = function () {
+            return sectorWidth;
+        };
+        
+        this.GetSectorHeight = function () {
+            return sectorHeight;
+        };
+        
+        this.GetGradient = function () {
+            return gradient;
+        };
+        
+        this.IsValidCoordinate = function (posX, posY) {
+            return posX >= 0 && posY >= 0 
+                    && posY <= gridSizeY && posX <= columns[posY % 2] - 1;
+        }
+    };
+    
+    var GameWorld = new TGameWorld();
+    
+    function THexagonField() {
+        var hexagonGroup = Game.add.group();
+		Game.stage.backgroundColor = "#ffffff";
+	    for (var i = 0; i < GameWorld.GetGridSizeY() / 2; i++) {
+			for (var j = 0; j < GameWorld.GetGridSizeX(); j++) {
+				if (GameWorld.GetGridSizeY() % 2 == 0 
+                    || i + 1 < GameWorld.GetGridSizeY() / 2 
+                    || j % 2==0) {
+					var hexagonX = GameWorld.GetHexagonWidth() * j / 2;
+					var hexagonY = GameWorld.GetHexagonHeight() * i * 1.5
+                                    + (GameWorld.GetHexagonHeight() / 4 * 3) * (j % 2);	
+					var hexagon = Game.add.sprite(hexagonX,hexagonY,"hexagon");
 					hexagonGroup.add(hexagon);
 				}
 			}
 		}
-		hexagonGroup.x = (game.width-hexagonWidth*Math.ceil(gridSizeX/2))/2;
-       	if(gridSizeX%2==0){
-        	hexagonGroup.x-=hexagonWidth/4;
+        
+		hexagonGroup.x = (Game.width - GameWorld.GetHexagonWidth() * Math.ceil(GameWorld.GetGridSizeX() / 2)) / 2;
+       	if (GameWorld.GetGridSizeX() % 2 == 0) {
+        	hexagonGroup.x -= GameWorld.GetHexagonWidth() / 4;
         }
-		hexagonGroup.y = (game.height-Math.ceil(gridSizeY/2)*hexagonHeight-Math.floor(gridSizeY/2)*hexagonHeight/2)/2;
-        if(gridSizeY%2==0){
-        	hexagonGroup.y-=hexagonHeight/8;
+       
+		hexagonGroup.y = (Game.height - Math.ceil(GameWorld.GetGridSizeY() / 2) * GameWorld.GetHexagonHeight() - Math.floor(GameWorld.GetGridSizeY() / 2)*GameWorld.GetHexagonHeight()/2)/2;
+        if (GameWorld.GetGridSizeY() % 2 == 0) {
+        	hexagonGroup.y -= GameWorld.GetHexagonHeight() / 8;
         }
-		marker = game.add.sprite(0,0,"marker");
+        
+        this.Add = function (marker) {
+            hexagonGroup.add(marker);
+        }
+        
+        this.FindHex = function () {
+            var candidateX = Math.floor((Game.input.worldX - hexagonGroup.x) / GameWorld.GetSectorWidth());
+            var candidateY = Math.floor((Game.input.worldY-hexagonGroup.y) / GameWorld.GetSectorHeight());
+            var deltaX = (Game.input.worldX-hexagonGroup.x) % GameWorld.GetSectorWidth();
+            var deltaY = (Game.input.worldY-hexagonGroup.y) % GameWorld.GetSectorHeight(); 
+            if(candidateY%2==0){
+            	if(deltaY<((GameWorld.GetHexagonHeight()/4)-deltaX*GameWorld.GetGradient())){
+                    candidateX--;
+                    candidateY--;
+                }
+                if(deltaY<((-GameWorld.GetHexagonHeight()/4)+deltaX*GameWorld.GetGradient())){
+                    candidateY--;
+                }
+            } else {
+                if(deltaX>=GameWorld.GetHexagonWidth()/2){
+                    if(deltaY<(GameWorld.GetHexagonHeight()/2-deltaX*GameWorld.GetGradient())){
+                	   candidateY--;
+                    }
+                } else {
+                    if(deltaY<deltaX*GameWorld.GetGradient()){
+                	   candidateY--;
+                    } else {
+                       candidateX--;
+                    }
+                }
+            }
+            return {
+                x: candidateX, 
+                y: candidateY
+            };
+        }
+    }
+    
+    var HexagonField;
+    
+    function TMarker() {
+        var marker = Game.add.sprite(0,0,"marker");
 		marker.anchor.setTo(0.5);
-		marker.visible=false;
-		hexagonGroup.add(marker);
-		game.input.mouse.mouseDownCallback = function(e){        
-			if(game.input.mouse.button == Phaser.Mouse.LEFT_BUTTON) { //Left Click
-				var hex = findHex(); 
-				placeMarker(hex.x, hex.y); 
+		marker.visible = false;
+		HexagonField.Add(marker);
+		
+        var _this = this;
+        Game.input.mouse.mouseDownCallback = function(e) {        
+			if (Game.input.mouse.button == Phaser.Mouse.LEFT_BUTTON) { //Left Click
+				var hex = HexagonField.FindHex(); 
+				_this.SetNewPosition(hex.x, hex.y); 
 			} else {
 				//Right Click	
 			}
 		};
-        moveIndex = game.input.addMoveCallback(findHex, this);
-	}
         
-	function findHex(){
-		var candidateX = Math.floor((game.input.worldX-hexagonGroup.x)/sectorWidth);
-        var candidateY = Math.floor((game.input.worldY-hexagonGroup.y)/sectorHeight);
-        var deltaX = (game.input.worldX-hexagonGroup.x)%sectorWidth;
-        var deltaY = (game.input.worldY-hexagonGroup.y)%sectorHeight; 
-        if(candidateY%2==0){
-        	if(deltaY<((hexagonHeight/4)-deltaX*gradient)){
-            	candidateX--;
-                candidateY--;
-            }
-            if(deltaY<((-hexagonHeight/4)+deltaX*gradient)){
-            	candidateY--;
-            }
-        } else {
-        	if(deltaX>=hexagonWidth/2){
-            	if(deltaY<(hexagonHeight/2-deltaX*gradient)){
-                	candidateY--;
-                }
-            } else {
-            	if(deltaY<deltaX*gradient){
-                	candidateY--;
+        this.SetNewPosition = function (posX, posY) {
+            if (!GameWorld.IsValidCoordinate(posX, posY)) {
+                marker.visible = false;
+		    } else {
+                marker.visible = true;
+                marker.x = GameWorld.GetHexagonWidth() * posX;
+                marker.y = GameWorld.GetHexagonHeight() / 4 * 3 * posY 
+                            + GameWorld.GetHexagonHeight() / 2;
+                if (posY % 2 == 0) {
+                    marker.x += GameWorld.GetHexagonWidth() / 2;
                 } else {
-                	candidateX--;
+                    marker.x += GameWorld.GetHexagonWidth();
                 }
             }
-        }
-		return { 
-        	x: candidateX, 
-          	y: candidateY
         };
+    }
+	
+    var Marker;
+    
+	function onPreload() {
+		Game.load.image("hexagon", "arts/hexagon.png");
+		Game.load.image("marker", "arts/marker.png");
 	}
-     
-    function placeMarker(posX,posY){
-		if(posX<0 || posY<0 || posY>=gridSizeY || posX>columns[posY%2]-1){
-			marker.visible=false;
-		} else {
-			marker.visible=true;
-			marker.x = hexagonWidth*posX;
-			marker.y = hexagonHeight/4*3*posY+hexagonHeight/2;
-			if(posY%2==0){
-				marker.x += hexagonWidth/2;
-			} else {
-				marker.x += hexagonWidth;
-			}
-		}
+
+	function onCreate() {
+        HexagonField = new THexagonField();
+        Marker = new TMarker();
 	}
 }
