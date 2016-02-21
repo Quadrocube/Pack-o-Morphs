8e2a01dd27471d14c0a50d06671fbf94e7fe2b04
Kamil Talipov <kamiltalipov@gmail.com>
2016-02-22 02:44:42 +0300

    Return myhold

--- client/game.js
+++ client/game.js
@@ -771,6 +771,10 @@ window.onload = function() {
             if (key.keyCode == Phaser.Keyboard.SPACEBAR) {
                 socket.emit('client_data', HexagonField.Dump2JSON());
             }
+            
+            if (key.keyCode === Phaser.Keyboard.ONE) {
+                TurnState.MyTurn();
+            }
         };
 	}
 	
