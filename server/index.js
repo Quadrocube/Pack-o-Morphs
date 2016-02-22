b9d0e8bd906efbdde927b681e3e1fcfb014e8ab2
Quadrocube <gliderok@gmail.com>
2016-02-21 23:09:01 -0600

    server side

--- server/index.js
+++ server/index.js
@@ -22,10 +22,99 @@ http.listen(3000, function(){
 });
 
 var listener = io.listen(http);
+
+function TQueue () {
+    this.carry = [];
+    this.length = function () {
+        return this.carry.length;
+    }
+    this.push = function (val) {
+        this.carry.push(val);
+    };
+    this.pop = function () {
+        this.carry.splice(0,1);
+    };
+    this.removeByValue = function (val) {
+        var found = undefined;
+        for (i in this.carry) {
+            if (this.carry[i] === val) {
+                found = i;
+            }
+        }
+        if (found !== undefined) {
+            this.carry.splice(found, 1);
+        }
+    };
+}
+
+var UserQueue = new TQueue();
+var lobbies = {};
+
+function Lobby(socketOne, socketTwo) {
+    this.socketOne = socketOne;
+    this.socketTwo = socketTwo;
+    this.socketOne.emit('found-opp', {'order': [0, 1]});
+    this.socketTwo.emit('found-opp', {'order': [1, 0]});
+
+    this._getOpp = function(id) {
+      if (id === socketOne.id) {
+        return socketTwo;
+      } else {
+        return socketOne;
+      }
+    }
+    
+    this.disconnect() = function(id) {
+      this._getOpp(id).disconnect();
+    }
+    
+    this.send = function(id, data) {
+      this._getOpp(id).emit('new-turn', data);
+    }
+};
+
+function initSession(playerOneSocket, playerTwoSocket) {
+  var lobby = new Lobby(playerOneSocket, playerTwoSocket);
+  lobbies[playerOneSocket.id] = lobby;
+  lobbies[playerTwoSocket.id] = lobby;
+};
+
 listener.sockets.on('connection', function(socket){
     console.log('user connected');
-    socket.on('client_data', function(data) {
-      process.stdout.write(JSON.stringify(data.objects) + "\n");
+    lobbies[socket.id] = undefined;
+
+    socket.on('manual-field-send', function(data) {
+      process.stdout.write("uid " + socket.id + ": " + JSON.stringify(data.objects) + "\n");
     });
+
+    socket.emit('found-opp', {'order': [0, 1]}); // REMOVE!
+
+    socket.on('disconnect', function(){
+      if (!lobbies[socket.id]) {
+        UserQueue.removeByValue(socket);
+      } else {
+        lobbies[socket.id].disconnect(socket.id);
+        delete lobbies[socket.id];
+      }
+    });
+
+    socket.on('new-turn', function(data) {
+      socket.emit('new-turn', data); // REMOVE!
+      console.log('new-turn');
+      /*
+      if (!lobbies[socket.id]) {
+        console.log("Panic! No lobby for user " + socket.id + ", but he is sending us new-turns!");
+      } else {
+        lobbies[socket.id].send(socket.id, data);
+      }
+      */
+    });
+
+    /*if (UserQueue.length() > 0) {
+      opp = UserQueue.pop();
+      initSession(socket, opp);
+    } else {
+      UserQueue.push(socket);
+    }*/
 });
 
